6daac8541540e8cbd4d5032a7fb1aa96e99d2838
ahm-ahm <ahm.ahm7785@gmail.com>
2023-01-09 16:11:38 +0500

    refreshed code

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:stream_demo/custom-stream/broadcast_stream_screen.dart';
 import 'package:stream_demo/stream-dynamically-widgets-adding/dynamic_stream_screen.dart';
-import 'package:stream_demo/stream-widgets-adding/stream_widgets_adding_screen.dart';
 
 import 'custom-stream/single_subscription_stream_screen.dart';
 
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StreamWidgetsAddingScreen(),
+      home: const DynamicStreamScreen(),
     );
   }
 }
